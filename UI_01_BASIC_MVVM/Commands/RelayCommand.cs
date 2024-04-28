9313ccac54dc87000eca75cd793d48afbac0f595
Mathias Sønderskov Schaltz <19936679+Sonderwoods@users.noreply.github.com>
2024-04-28 22:48:21 +0200

    Create RelayCommand.cs

---
+++ UI_01_BASIC_MVVM/Commands/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace UI_01_BASIC_MVVM.Commands
+{
+
+    /// <summary>
+    /// A relay command is a command where we can set the method on instanciation.
+    /// This creates a more flexible command than creating a new command for each method.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+
+        private readonly Action mAction;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; } // These values make sure that CanExecute is executed
+            remove { CommandManager.RequerySuggested -= value; } 
+        }
+
+        public RelayCommand(Action action)
+        {
+            mAction = action;
+        }
+
+        public virtual bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            mAction.Invoke();
+        }
+
+
+    }
+}
